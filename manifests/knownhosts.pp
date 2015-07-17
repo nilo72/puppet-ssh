2ef5e736780c25db28b5c31c3e29d83a87824bb7
Oliver Neumann <oliver.neumann@haw-hamburg.de>
2015-07-17 13:22:34 +0200

    both types are relevant

--- manifests/knownhosts.pp
+++ manifests/knownhosts.pp
@@ -1,6 +1,6 @@
 class ssh::knownhosts {
   if( $::operatingsystem == 'Debian' and $::lsbmajdistrelease < 8 ){
-    Sshkey <<| type != 'ssh-ed25519' |>>
+    Sshkey <<| type != 'ssh-ed25519' or type != 'ed25519' |>>
   }
   else{
     Sshkey <<| |>>
